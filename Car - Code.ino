7fbf496a5b7c171ffbf6d6b89be34a53c23f532c
Hassaan Nadeem <72746343+hnadeem03@users.noreply.github.com>
2022-05-23 20:26:43 +0500

    Update Car - Code.ino

--- Car - Code.ino
+++ Car - Code.ino
@@ -14,6 +14,9 @@
 long duration;
 int distance;
 
+int left_signal;
+int right_signal;
+
 int calc_distance();
 
 void setup() {
@@ -32,43 +35,27 @@ void setup() {
 
 void loop() {
   distance = calc_distance();
-  Serial.println(distance);
+  left_signal = digitalRead(left_infr);
+  right_signal = digitalRead(right_infr);
   Serial.println("Left: ");
   Serial.println(digitalRead(left_infr));
   Serial.println("Right: ");
   Serial.println(digitalRead(right_infr));
-  if(distance <= 20 && distance >= 10 && digitalRead(left_infr) == LOW && digitalRead(right_infr) == LOW){ //go straight - follow hand
-    digitalWrite(left_forward, HIGH);
-    digitalWrite(right_forward, HIGH);
-    digitalWrite(left_backward, LOW);
-    digitalWrite(right_backward, LOW);
-    delay(200);
-    Serial.println("Straight");
-  }
-  else if(distance < 10){
-    digitalWrite(left_forward, LOW);
-    digitalWrite(right_forward, HIGH);
-    digitalWrite(left_backward, HIGH);
-    digitalWrite(right_backward, LOW);
-    delay(200); //reverse to stop momentem
-    Serial.println("Stop");
+  
+  if (distance < 10){
+    stop();
   }
   
-  else if(digitalRead(left_infr) == HIGH && digitalRead(right_infr) == LOW){ //Turn Left
-    digitalWrite(left_forward, LOW);
-    digitalWrite(right_forward, HIGH);
-    digitalWrite(left_backward, HIGH);
-    digitalWrite(right_backward, LOW);
-    delay(200);
-    Serial.println("Left");
+  else if(distance <= 20 && distance >= 10 && left_signal == 1 && right_signal == 1){ //go straight - follow hand
+    straight();
   }
-  else if(digitalRead(right_infr) == HIGH && digitalRead(left_infr) == LOW){  //Turn Right
-    digitalWrite(left_forward, HIGH);
-    digitalWrite(right_forward, LOW);
-    digitalWrite(left_backward, LOW);
-    digitalWrite(right_backward, HIGH);
-    delay(200);
-    Serial.println("Right");
+    
+  else if(left_signal == 0 && right_signal == 1){ //Turn right
+    right();
+  }
+  
+  else if(right_signal == 0 && left_signal == 1){  //Turn left
+    left();
   }
 
   reset();
@@ -88,8 +75,45 @@ int calc_distance(){
  
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   //distance contains distance in centimeter
+  Serial.println("Distance: ");
   Serial.println(distance);
   return distance;
+}
+
+void straight(){
+  digitalWrite(left_forward, HIGH);
+  digitalWrite(right_forward, HIGH);
+  digitalWrite(left_backward, LOW);
+  digitalWrite(right_backward, LOW);
+  delay(200);
+  Serial.println("Straight");
+}
+
+void left(){
+  digitalWrite(left_forward, LOW);
+  digitalWrite(right_forward, HIGH);
+  digitalWrite(left_backward, HIGH);
+  digitalWrite(right_backward, LOW);
+  delay(200);
+  Serial.println("Left");
+}
+
+void right(){
+  digitalWrite(left_forward, HIGH);
+  digitalWrite(right_forward, LOW);
+  digitalWrite(left_backward, LOW);
+  digitalWrite(right_backward, HIGH);
+  delay(200);
+  Serial.println("Right");
+}
+
+void stop(){
+  digitalWrite(left_forward, LOW);
+  digitalWrite(right_forward, HIGH);
+  digitalWrite(left_backward, HIGH);
+  digitalWrite(right_backward, LOW);
+  delay(200); //reverse to stop momentem
+  Serial.println("Stop");
 }
 
 void reset(){
